5dd20cdc57263a13e488ddde16e9407dc9cb9275
Alaska Websites <142437043+AlaskaWebsites@users.noreply.github.com>
2026-09-17 22:09:20 -0300

    feat(modules): importar ProfessionalModule no AppModule (ADR 011)

--- apps/api/src/infrastructure/modules/app.module.ts
+++ apps/api/src/infrastructure/modules/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common'
-import { HealthController } from '../http/controllers/health.controller'
 import { DatabaseModule } from './database.module'
 import { TenantModule } from './tenant.module'
 import { PixModule } from './pix.module'
 import { OrderModule } from './order.module'
 import { BookingModule } from './booking.module'
 import { ProductModule } from './product.module'
+import { ProfessionalModule } from './professional.module'
+import { HealthController } from '../http/controllers/health.controller'
 
 @Module({
-  imports: [DatabaseModule, TenantModule, PixModule, OrderModule, BookingModule, ProductModule],
+  imports: [
+    DatabaseModule,
+    TenantModule,
+    PixModule,
+    OrderModule,
+    BookingModule,
+    ProductModule,
+    ProfessionalModule
+  ],
   controllers: [HealthController]
 })
 export class AppModule {}
